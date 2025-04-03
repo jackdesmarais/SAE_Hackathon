8962e79923a8d5f873501ac5fbc679b8a9d1507c
JJD elzar <desmara@cshl.edu>
2025-04-03 12:06:03 -0400

    updating output name

--- batch_topK_hyperParam_sweep/training_scripts/launch_speed_test.sh
+++ batch_topK_hyperParam_sweep/training_scripts/launch_speed_test.sh
@@ -11,7 +11,7 @@ for k in ${ks[@]}; do
                 # Take min of topk_aux and dict_size-k to ensure enough auxiliary features
                 topk_aux=$(( topk_aux < (dict_size-k) ? topk_aux : (dict_size-k) ))
                 sbatch \
-                --job-name="v4_w${workers}_testing_speed_batchtopk_HP_sweep_k${k}_dict${dict_size}_topkaux${topk_aux}_auxpen${aux_penalty}" \
+                --job-name="v5_w${workers}_testing_speed_batchtopk_HP_sweep_k${k}_dict${dict_size}_topkaux${topk_aux}_auxpen${aux_penalty}" \
                 batch_topK_hyperParam_sweep/training_scripts/training_job.sh ${k} ${dict_size} ${topk_aux} ${aux_penalty} ${workers}
             done
         done
