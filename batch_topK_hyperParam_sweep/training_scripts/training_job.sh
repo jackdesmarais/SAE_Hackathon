cffee38ce494d2c6b5a6e106fc67ddec6038ad52
JJD elzar <desmara@cshl.edu>
2025-04-03 11:27:53 -0400

    prepping a worker number test with the new code

--- batch_topK_hyperParam_sweep/training_scripts/training_job.sh
+++ batch_topK_hyperParam_sweep/training_scripts/training_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --output=./batch_topK_hyperParam_sweep/logs/V4/%x_qsub.sh.stout.%A.%a      # Output file
 #SBATCH --error=./batch_topK_hyperParam_sweep/logs/V4/%x_qsub.sh.sterr.%A.%a      # Error file
 #SBATCH --time=11:59:59             # Max time for the job (max 2 hours for the fast queue or 12 hours for the default queue)
-#SBATCH --mem=200G                   # Memory request
+#SBATCH --mem=250G                   # Memory request
 #SBATCH --ntasks=1                  # The number of parallel tasks in the job
 #SBATCH --cpus-per-task=24           # Number of CPUs
 #SBATCH --gpus-per-task=1           # Number of GPUs
@@ -19,6 +19,7 @@ topk=$1
 dict_size=$2
 topk_aux=$3
 aux_penalty=$4
+workers=$5
 
 python ./Train_SpliceAI_WG_SAE.py --act-size 32 \
                             --sae-type batch_topk \
@@ -32,6 +33,6 @@ python ./Train_SpliceAI_WG_SAE.py --act-size 32 \
                             --exp-name "v4_testing_speed_batchtopk_HP_sweep" \
                             --overwite-name "SpliceAI_WG_Add_14_MB_3_out_topk${topk}_dict${dict_size}_topkaux${topk_aux}_auxpen${aux_penalty}" \
                             --outpath ./batch_topK_hyperParam_sweep/out/V4/ \
-                            --num-workers 1 
+                            --num-workers ${workers}
 
                             
