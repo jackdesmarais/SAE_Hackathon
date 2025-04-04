e72f6af7a3560d4b75a424ef8375622e025f93e6
JJD elzar <desmara@cshl.edu>
2025-04-04 15:40:50 -0400

    prepping a new sweep trying to revive dead latents and train faster

--- batch_topK_hyperParam_sweep/training_scripts/launch_hpsweep.sh
+++ batch_topK_hyperParam_sweep/training_scripts/launch_hpsweep.sh
@@ -1,17 +1,19 @@
 
-ks=(4 8 16 32)
-dict_sizes=(256 512 1024)
-topk_auxs=(4 8 16 32 128 256)
-
+ks=(16 24)
+dict_sizes=(256 512)
+topk_auxs=(16 32 128)
+workers=16
+aux_penalties=(1 0.5 0.1 0.01)
 for k in ${ks[@]}; do
     for dict_size in ${dict_sizes[@]}; do
         for topk_aux in ${topk_auxs[@]}; do
-            aux_penalty=$(echo "scale=4; 1/$k" | bc)
-            # Take min of topk_aux and dict_size-k to ensure enough auxiliary features
-            topk_aux=$(( topk_aux < (dict_size-k) ? topk_aux : (dict_size-k) ))
-            sbatch \
-                --job-name="v4_testing_speed_batchtopk_HP_sweep_k${k}_dict${dict_size}_topkaux${topk_aux}_auxpen${aux_penalty}" \
-                batch_topK_hyperParam_sweep/training_scripts/training_job.sh ${k} ${dict_size} ${topk_aux} ${aux_penalty}
+            for aux_penalty in ${aux_penalties[@]}; do
+                # Take min of topk_aux and dict_size-k to ensure enough auxiliary features
+                topk_aux=$(( topk_aux < (dict_size-k) ? topk_aux : (dict_size-k) ))
+                sbatch \
+                    --job-name="v7_hp_sweep_k${k}_dict${dict_size}_topkaux${topk_aux}_auxpen${aux_penalty}" \
+                    batch_topK_hyperParam_sweep/training_scripts/training_job.sh ${k} ${dict_size} ${topk_aux} ${aux_penalty} ${workers}
+            done
         done
     done
 done
--- batch_topK_hyperParam_sweep/training_scripts/training_job.sh
+++ batch_topK_hyperParam_sweep/training_scripts/training_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --output=./batch_topK_hyperParam_sweep/logs/V6/%x_qsub.sh.stout.%A.%a      # Output file
-#SBATCH --error=./batch_topK_hyperParam_sweep/logs/V6/%x_qsub.sh.sterr.%A.%a      # Error file
+#SBATCH --output=./batch_topK_hyperParam_sweep/logs/V7/%x_qsub.sh.stout.%A.%a      # Output file
+#SBATCH --error=./batch_topK_hyperParam_sweep/logs/V7/%x_qsub.sh.sterr.%A.%a      # Error file
 #SBATCH --time=11:59:59             # Max time for the job (max 2 hours for the fast queue or 12 hours for the default queue)
 #SBATCH --mem=250G                   # Memory request
 #SBATCH --ntasks=1                  # The number of parallel tasks in the job
@@ -29,9 +29,9 @@ python ./Train_SpliceAI_WG_SAE.py --act-size 32 \
                             --n-batches-to-dead 5 \
                             --top-k-aux ${topk_aux} \
                             --aux-penalty ${aux_penalty} \
-                            --exp-name "v6_testing_no_preload_batchtopk_HP_sweep" \
+                            --exp-name "v7_hp_sweepHP_sweep" \
                             --overwite-name "SpliceAI_WG_Add_14_MB_3_out_topk${topk}_dict${dict_size}_topkaux${topk_aux}_auxpen${aux_penalty}" \
-                            --outpath ./batch_topK_hyperParam_sweep/out/V6/ \
+                            --outpath ./batch_topK_hyperParam_sweep/out/V7/ \
                             --num-workers ${workers} #\
                             # --preload-data
 
