f42c7c8180ae87b4e5a35b3f39ff40d112cc07f8
JJD elzar <desmara@cshl.edu>
2025-04-12 13:41:35 -0400

    testing larger worker nums

--- batch_topK_hyperParam_sweep/training_scripts/launch_speed_test.sh
+++ batch_topK_hyperParam_sweep/training_scripts/launch_speed_test.sh
@@ -2,7 +2,7 @@
 ks=(16)
 dict_sizes=(1024)
 topk_auxs=(16)
-workers=(0 2 4 8 16 32 50)
+workers=(0 2 4 8 16 32 50 64 128)
 for k in ${ks[@]}; do
     for dict_size in ${dict_sizes[@]}; do
         for topk_aux in ${topk_auxs[@]}; do
